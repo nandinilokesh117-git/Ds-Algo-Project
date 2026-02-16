33a3cff65e51c0e8bae65c69be5baf3f08dbfa42
Vijayarani Packiaraj <vijayaranipackiaraj1427@gmail.com>
2026-02-15 22:34:37 -0600

    removed comment lines in excelreader.

--- utils/excelReader.js
+++ utils/excelReader.js
@@ -4,11 +4,5 @@ export function readExcel(filePath, sheetName) {
     const workbook = XLSX.readFile(filePath);
     const worksheet = workbook.Sheets[sheetName];
     const data = XLSX.utils.sheet_to_json(worksheet);
-
-//     // defval:'' keeps empty cells, raw:false preserves line breaks
-//   const data = xlsx.utils.sheet_to_json(sheet, {
-//     defval: '',
-//     raw: false
-//   });
     return data;  // returns an array of objects
 }
